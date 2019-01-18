caab08de1023990e9c1dd5ccbf56182571ca7cad
Tyler Conklin <tylerconklin@dhcp-10-134-193-86.uawifi.arizona.edu>
2019-01-17 18:11:09 -0700

    Add drill02

---
+++ src/edu/spr19cs210/drill02/Drill02.java
@@ -0,0 +1,34 @@
+package edu.spr19cs210.drill02;
+
+public class Drill02 {
+
+    // takes a number of characters to print as a parameter
+    // returns a string with one or two asterisks in the middle
+    // surrounded by alligators
+    public static String zigzag(int n) {
+        return "";
+    }
+
+    // takes a string and a character as parameters and returns 
+    // the string with all copies of the character moved to the 
+    // end and capitalized
+    public static String moveToEnd(String s, char c) {
+        return "";
+    }
+
+    // takes a string and two characters as parameters
+    // returns a string that is the same as the passed in string
+    // except that all occurrences of the first character are replaced
+    // with the second
+    public static String replaceAll(String s, char from, char to) {
+        return "";
+    }
+
+    // takes an integer as a parameter
+    // returns true if the digits are in sorted order ascending, 
+    // false otherwise
+    public static boolean digitsSorted(int x) {
+        return false;
+    }
+
+}
---
+++ src/edu/spr19cs210/drill02/Drill02TestClass.java
@@ -0,0 +1,271 @@
+package edu.spr19cs210.drill02;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+// This is an annotation for assigning point values to tests
+import com.gradescope.jh61b.grader.GradedTest;
+
+
+public class Drill02TestClass {
+
+    @Test
+    @GradedTest(name = "zigzag test1, n=1", max_score = 1)
+    public void test_zigzag_test1() {
+        String x = Drill02.zigzag(1);
+        System.out.println("zigzag(1), got " + x);
+        assertEquals(x, "*");
+    }
+
+    @Test
+    @GradedTest(name = "zigzag test2, n=2", max_score = 1)
+    public void test_zigzag_test2() {
+        String x = Drill02.zigzag(2);
+        System.out.println("zigzag(2), got " + x);
+        assertEquals(x, "**");
+    }
+
+    @Test
+    @GradedTest(name = "zigzag test3, n=3", max_score = 1)
+    public void test_zigzag_test3() {
+        String x = Drill02.zigzag(3);
+        System.out.println("zigzag(3), got " + x);
+        assertEquals(x, "<*>");
+    }
+
+    @Test
+    @GradedTest(name = "zigzag test4, n=4", max_score = 1)
+    public void test_zigzag_test4() {
+        String x = Drill02.zigzag(4);
+        System.out.println("zigzag(4), got " + x);
+        assertEquals(x, "<**>");
+    }
+
+    @Test
+    @GradedTest(name = "zigzag test5, n=5", max_score = 1)
+    public void test_zigzag_test5() {
+        String x = Drill02.zigzag(5);
+        System.out.println("zigzag(5), got " + x);
+        assertEquals(x, "<<*>>");
+    }
+
+    @Test
+    @GradedTest(name = "zigzag test6, n=6", max_score = 1)
+    public void test_zigzag_test6() {
+        String x = Drill02.zigzag(6);
+        System.out.println("zigzag(6), got " + x);
+        assertEquals(x, "<<**>>");
+    }
+
+    @Test
+    @GradedTest(name = "zigzag test7, n=7", max_score = 1)
+    public void test_zigzag_test7() {
+        String x = Drill02.zigzag(7);
+        System.out.println("zigzag(7), got " + x);
+        assertEquals(x, "<<<*>>>");
+    }
+
+    @Test
+    @GradedTest(name = "zigzag test8, n=8", max_score = 1)
+    public void test_zigzag_test8() {
+        String x = Drill02.zigzag(8);
+        System.out.println("zigzag(8), got " + x);
+        assertEquals(x, "<<<**>>>");
+    }
+
+    @Test
+    @GradedTest(name = "zigzag test9, n=9", max_score = 1)
+    public void test_zigzag_test9() {
+        String x = Drill02.zigzag(9);
+        System.out.println("zigzag(9), got " + x);
+        assertEquals(x, "<<<<*>>>>");
+    }
+
+    @Test
+    @GradedTest(name = "zigzag test10, n=10", max_score = 1)
+    public void test_zigzag_test10() {
+        String x = Drill02.zigzag(10);
+        System.out.println("zigzag(10), got " + x);
+        assertEquals(x, "<<<<**>>>>");
+    }
+
+    @Test
+    @GradedTest(name = "moveToEnd test1, s=hello, c=l", max_score = 1)
+    public void test_moveToEnd_test1() {
+        String x = Drill02.moveToEnd("hello", 'l');
+        System.out.println("moveToEnd(hello,l), got " + x);
+        assertEquals(x, "heoLL");
+    }
+
+    @Test
+    @GradedTest(name = "moveToEnd test2, s=hello, c=e", max_score = 1)
+    public void test_moveToEnd_test2() {
+        String x = Drill02.moveToEnd("hello", 'e');
+        System.out.println("moveToEnd(hello,e), got " + x);
+        assertEquals(x, "hlloE");
+    }
+
+    @Test
+    @GradedTest(name = "moveToEnd test3, s=hello there, c=e", max_score = 1)
+    public void test_moveToEnd_test3() {
+        String x = Drill02.moveToEnd("hello there", 'e');
+        System.out.println("moveToEnd(hello there,e), got " + x);
+        assertEquals(x, "hllo thrEEE");
+    }
+
+    @Test
+    @GradedTest(name = "moveToEnd test4, s=hello there, c=q", max_score = 1)
+    public void test_moveToEnd_test4() {
+        String x = Drill02.moveToEnd("hello there", 'q');
+        System.out.println("moveToEnd(hello there,q), got " + x);
+        assertEquals(x, "hello there");
+    }
+
+    @Test
+    @GradedTest(name = "moveToEnd test5, s=HELLO there, c=e", max_score = 1)
+    public void test_moveToEnd_test5() {
+        String x = Drill02.moveToEnd("HELLO there", 'e');
+        System.out.println(
+                "moveToEnd(HELLO there,e), got " + x);
+        assertEquals(x, "HLLO thrEEE");
+    }
+
+    @Test
+    @GradedTest(name = "moveToEnd test6, s=HHH, c=H", max_score = 1)
+    public void test_moveToEnd_test6() {
+        String x = Drill02.moveToEnd("HHH", 'H');
+        System.out.println(
+                "moveToEnd(HHH,H), got " + x);
+        assertEquals(x,"HHH");
+    }
+
+    @Test
+    @GradedTest(name = "moveToEnd test7, s=hahahaha, c=h", max_score = 1)
+    public void test_moveToEnd_test7() {
+        String x = Drill02.moveToEnd("hahahaha", 'h');
+        System.out.println(
+                "moveToEnd(hahahaha,h), got " + x);
+        assertEquals(x, "aaaaHHHH");
+    }
+
+    @Test
+    @GradedTest(name = "replaceAll test1, s1=Merlin, c1=e, c2=a", max_score = 1)
+    public void test_replaceAll_test1() {
+        String x = Drill02.replaceAll("Merlin", 'e', 'a');
+        System.out.println("replaceAll(Merlin,e,a), got " + x);
+        assertEquals(x, "Marlin");
+    }
+
+    @Test
+    @GradedTest(name = "replaceAll test2, s1=Merlin, c1=q, c2=a", max_score = 1)
+    public void test_replaceAll_test2() {
+        String x = Drill02.replaceAll("Merlin", 'q', 'a');
+        System.out.println("replaceAll(Merlin,q,a), got " + x);
+        assertEquals(x, "Merlin");
+    }
+
+    @Test
+    @GradedTest(name = "replaceAll test3, s1=Purrcy, c1=r, c2=a", max_score = 1)
+    public void test_replaceAll_test3() {
+        String x = Drill02.replaceAll("Purrcy", 'r', 'a');
+        System.out.println("replaceAll(Purrcy,r,a), got " + x);
+        assertEquals(x, "Puaacy");
+    }
+
+    @Test
+    @GradedTest(name = "replaceAll test4, s1=Chester, c1=e, c2=m", max_score = 1)
+    public void test_replaceAll_test4() {
+        String x = Drill02.replaceAll("Chester", 'e', 'm');
+        System.out.println("replaceAll(Chester,e,m), got " + x);
+        assertEquals(x, "Chmstmr");
+    }
+
+    @Test
+    @GradedTest(name = "replaceAll test5, s1=ChEster, c1=e, c2=m", max_score = 1)
+    public void test_replaceAll_test5() {
+        String x = Drill02.replaceAll("ChEster", 'e', 'm');
+        System.out.println("replaceAll(ChEster,e,m), got " + x);
+        assertEquals(x, "ChEstmr");
+    }
+
+    @Test
+    @GradedTest(name = "digitsSorted test1, x=0", max_score = 1)
+    public void test_digitsSorted_test1() {
+        boolean x = Drill02.digitsSorted(0);
+        System.out.println("digitsSorted(0), got " + x);
+        assertEquals(x, true);
+    }
+
+    @Test
+    @GradedTest(name = "digitsSorted test2, x=234", max_score = 1)
+    public void test_digitsSorted_test2() {
+        boolean x = Drill02.digitsSorted(234);
+        System.out.println("digitsSorted(234), got " + x);
+        assertEquals(x, true);
+    }
+
+    @Test
+    @GradedTest(name = "digitsSorted test3, x=-2345", max_score = 1)
+    public void test_digitsSorted_test3() {
+        boolean x = Drill02.digitsSorted(-2345);
+        System.out.println("digitsSorted(-2345), got " + x);
+        assertEquals(x, true);
+    }
+
+    @Test
+    @GradedTest(name = "digitsSorted test4, x=22334455", max_score = 1)
+    public void test_digitsSorted_test4() {
+        boolean x = Drill02.digitsSorted(22334455);
+        System.out.println("digitsSorted(22334455), got " + x);
+        assertEquals(x, true);
+    }
+
+    @Test
+    @GradedTest(name = "digitsSorted test5, x=-5", max_score = 1)
+    public void test_digitsSorted_test5() {
+        boolean x = Drill02.digitsSorted(-5);
+        System.out.println("digitsSorted(-5), got " + x);
+        assertEquals(x, true);
+    }
+
+    @Test
+    @GradedTest(name = "digitsSorted test6, x=4321", max_score = 1)
+    public void test_digitsSorted_test6() {
+        boolean x = Drill02.digitsSorted(4321);
+        System.out.println("digitsSorted(4321), got " + x);
+        assertEquals(x, false);
+    }
+
+    @Test
+    @GradedTest(name = "digitsSorted test7, x=24378", max_score = 1)
+    public void test_digitsSorted_test7() {
+        boolean x = Drill02.digitsSorted(24378);
+        System.out.println("digitsSorted(24378), got " + x);
+        assertEquals(x, false);
+    }
+
+    @Test
+    @GradedTest(name = "digitsSorted test8, x=21", max_score = 1)
+    public void test_digitsSorted_test8() {
+        boolean x = Drill02.digitsSorted(21);
+        System.out.println("digitsSorted(21), got " + x);
+        assertEquals(x, false);
+    }
+
+    @Test
+    @GradedTest(name = "digitsSorted test9, x=-33331", max_score = 1)
+    public void test_digitsSorted_test9() {
+        boolean x = Drill02.digitsSorted(-33331);
+        System.out.println("digitsSorted(-33331), got " + x);
+        assertEquals(x, false);
+    }
+
+    @Test
+    @GradedTest(name = "digitsSorted test5, x=-1233331", max_score = 1)
+    public void test_digitsSorted_test10() {
+        boolean x = Drill02.digitsSorted(-1233331);
+        System.out.println("digitsSorted(-1233331), got " + x);
+        assertEquals(x, false);
+    }
+}
